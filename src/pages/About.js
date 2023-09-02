08c3713812bbdf966c5e2c52ed9da078ebd6e10f
nawfal-ben <nawfal.bennaceur@gmail.com>
2023-09-02 13:58:51 +0100

    use react router older version

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos facilis, saepe impedit rem non totam eius ex, excepturi explicabo cupiditate quas corrupti natus necessitatibus sed quia omnis qui vel molestiae.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem velit soluta cupiditate deleniti doloribus, tenetur qui voluptate quisquam ipsum at saepe, accusamus aperiam culpa optio, quam harum temporibus molestias exercitationem.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere molestias ut ipsam quasi eveniet ducimus voluptatum quas atque! Modi rerum sit ea error minima sequi tempore molestias quam delectus vel!</p>
+        </>
+     );
+}
+ 
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return ( 
+        <>
+            <h2>Welcome</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quisquam asperiores reprehenderit in error laudantium odio consequuntur, et nihil dolore ea, quaerat aut, accusamus culpa nostrum. Neque perferendis quia quasi!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid dolorum harum nam asperiores ducimus earum soluta neque veritatis, inventore modi. Earum iusto numquam repudiandae tempora voluptatibus tenetur nam dolorem enim.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur deleniti laborum enim eveniet iusto sint quis maiores fuga, eius, at quibusdam doloribus cupiditate similique iste sapiente natus dolorem est obcaecati.</p>
+        </>
+     );
+}
+ 
+export default Home;
